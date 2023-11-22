31b5d1f732203741919c3ec118144285f2b65b7d
Muhamad Yoga Atmajaya <myogaatmajaya@gmail.com>
2023-11-22 13:32:55 +0700

    cd

--- app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,12 +11,22 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 // @react-native-firebase/app
 import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 // @react-native-firebase/auth
 import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
 // @react-native-google-signin/google-signin
 import com.reactnativegooglesignin.RNGoogleSigninPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-splash-screen
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+// react-native-svg
+import com.horcrux.svg.SvgPackage;
 
 public class PackageList {
   private Application application;
@@ -62,9 +72,14 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
       new ReactNativeFirebaseAppPackage(),
       new ReactNativeFirebaseAuthPackage(),
-      new RNGoogleSigninPackage()
+      new RNGoogleSigninPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new SplashScreenReactPackage(),
+      new SvgPackage()
     ));
   }
 }
